e06060036be9b1d55bd117785e3300c968d90a18
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2023-10-20 14:36:33 +0200

    Foundry fund me init

---
+++ foundry-fund-me/test/FundMeTest.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import {Test, console} from "forge-std/Test.sol";
+
+contract FundMeTest is Test {
+
+    function setUp() external {
+        console.log("CIAO");
+    }
+
+
+}
